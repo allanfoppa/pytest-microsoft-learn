83a45b82071a8eb78be9c013b888ae9f3acbf2a1
Allan Foppa <18150779+allanfoppa@users.noreply.github.com>
2023-07-05 13:12:57 -0300

    Test pokemon attribute with parametrize

--- tests/test_pokemon_attribute.py
+++ tests/test_pokemon_attribute.py
@@ -1,3 +1,4 @@
+import pytest
 
 HEIGHT = 5
 WEIGHT = 90
@@ -7,3 +8,13 @@ def test_pokemon_attribute(pokemon_info):
   assert pokemon_info['name'] == NAME
   assert pokemon_info['height'] == HEIGHT
   assert pokemon_info['weight'] == WEIGHT
+
+
+@pytest.mark.parametrize(
+  "attr_name, attr_value",
+  [("name", NAME), ("height", HEIGHT), ("weight", WEIGHT)]
+)
+def test_pokemon_attribute_with_parametrize(pokemon_info, attr_name, attr_value):
+  assert isinstance(pokemon_info, dict)
+  assert attr_name in pokemon_info
+  assert pokemon_info[attr_name] == attr_value
